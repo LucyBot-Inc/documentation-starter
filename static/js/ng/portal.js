58125f6a99bfb283544e1057f0a33baaee1741f1
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-08-10 15:08:47 +0000

    make markdown links open in new tab, add support for externalDocs

--- static/js/ng/portal.js
+++ static/js/ng/portal.js
@@ -1,11 +1,24 @@
+var maybeAddExternalDocs = function(description, externalDocs) {
+  if (!externalDocs || !externalDocs.url) return description;
+  if (description) description += '\n\n';
+  else description = '';
+  var url = document.createElement('a');
+  url.href = externalDocs.url;
+  description += 'Read more at [' + url.hostname + '](' + url.href + ')';
+  return description;
+}
+
 App.controller('Portal', function($scope) {
   $scope.routes = [];
+  var info = $scope.spec.info = $scope.spec.info || {};
+  info.description = maybeAddExternalDocs(info.description, $scope.spec.externalDocs);
   for (path in $scope.spec.paths) {
     var pathParams = $scope.spec.paths[path].parameters || [];
     for (method in $scope.spec.paths[path]) {
       if (method === 'parameters') continue;
       var operation = $scope.spec.paths[path][method];
       operation.parameters = (operation.parameters || []).concat(pathParams);
+      operation.description = maybeAddExternalDocs(operation.description, operation.externalDocs);
       var route = {path: path, method: method, operation: operation};
       route.visual = operation.responses['200'] && operation.responses['200']['x-lucy/view'];
       $scope.routes.push(route);
