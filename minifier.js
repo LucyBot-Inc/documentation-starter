64256430cc2ab772f9d1639cd949f9b3eb0ce413
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-08-14 22:11:03 +0000

    Minify on all requests in DEVELOPMENT

---
+++ minifier.js
@@ -0,0 +1,64 @@
+var FS = require('fs');
+var UglifyJS = require('uglify-js');
+var CleanCSS = require('clean-css');
+
+var Router = module.exports = require('express').Router();
+
+var toMinify = [{
+  fileIn: [
+    "static/bower/jquery/dist/jquery.min.js",
+    "static/bower/bootstrap/dist/js/bootstrap.min.js",
+    "static/bower/angular/angular.min.js",
+    "static/bower/highlightjs/highlight.pack.js",
+    "static/bower/zeroclipboard/dist/ZeroClipboard.min.js",
+    "static/bower/marked/lib/marked.js",
+    "static/bower/swagger-parser/dist/swagger-parser.min.js",
+    "static/js/examples.js",
+    "static/js/lucy.js",
+    "static/bower/angular-zeroclipboard/src/angular-zeroclipboard.js",
+    "static/bower/angular-highlightjs/angular-highlightjs.min.js",
+    "static/bower/angular-marked/angular-marked.js",
+    "static/js/ng/app.js",
+    "static/js/ng/portal.js",
+    "static/js/ng/documentation.js",
+    "static/js/ng/console.js",
+    "static/js/ng/oauth2.js",
+    "static/js/sort-routes.js",
+    "static/js/ng/parameter.js",
+    "static/js/ng/spec-url.js",
+  ],
+  fileOut: 'static/minified/js/all.js',
+}, {
+  fileIn: [
+    "static/less/styles.css",
+    "static/bower/highlightjs/styles/github.css",
+    "static/bower/highlightjs/styles/atelier-forest.light.css",
+    "static/css/portal.css",
+    "static/css/spec-url.css",
+    "static/css/verb-colors.css",
+    "static/css/documentation.css",
+    "static/css/console.css",
+  ],
+  fileOut: 'static/minified/css/all.css',
+  processor: function(code) { return new CleanCSS().minify(code).styles }
+}]
+
+var minify = function(m) {
+  var inFiles = m.fileIn.map(function(f) {
+    return FS.readFileSync(__dirname + '/' + f, 'utf8')
+  });
+  var concat = inFiles.join('\n');
+  if (m.processor) concat = m.processor(concat);
+  FS.writeFileSync(__dirname + '/' + m.fileOut, inFiles.join('\n'));
+};
+
+toMinify.forEach(minify);
+
+if (process.env.DEVELOPMENT) {
+  toMinify.forEach(function(m) {
+    Router.get(m.fileOut.substring(6), function(req, res, next) {
+      minify(m);
+      next();
+    })
+  })
+}
