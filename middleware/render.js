1df9239c2593db11b3863b792b707e5f18ce60fe
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-08-31 22:05:09 +0000

    start modularizing

--- middleware/render.js
+++ middleware/render.js
@@ -1,8 +1,14 @@
+var Path = require('path');
+var Jade = require('jade');
+
+var viewDir = Path.join(__dirname, '../views');
+
 module.exports = function(tmpl, inputs) {
+  var render = Jade.compile(Path.join(viewDir, tmpl + '.jade'))
   inputs = inputs || {};
   inputs.page = tmpl;
   inputs.angularImports = inputs.angularImports || [];
   return function(req, res) {
-    res.render(tmpl, inputs);
+    res.send(render(tmpl), inputs);
   }
 }
