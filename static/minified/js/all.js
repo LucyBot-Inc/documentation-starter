1438e0d2029fe03d5b23ab698b8e2607a0874b9d
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-09-04 01:19:21 +0000

    Full editor

--- static/minified/js/all.js
+++ static/minified/js/all.js
@@ -11344,6 +11344,7 @@ App.controller('Docs', function($scope) {
   }
   $scope.$watch('query', filterRoutes);
   $scope.$watch('activeTag', filterRoutes);
+  $scope.$watch('routes', filterRoutes);
   $scope.showRoute = function(route) {
     if (!$scope.query) return true;
     var query = $scope.query.toLowerCase();
@@ -11357,6 +11358,22 @@ App.controller('Docs', function($scope) {
   $scope.switchMode = function() {
     $scope.editorMode = !$scope.editorMode;
   }
+
+  $scope.addOperation = function() {
+    var path = '/newOperation';
+    var i = 0;
+    while ($scope.spec.paths[path]) path = '/newOperation' + (++i);
+    var op = $scope.spec.paths[path] = {
+      parameters: [],
+      responses: {
+        '200': {
+          description: 'Successful Response'
+        }
+      }
+    };
+    $scope.routes.push({operation: op, method: 'get', path: path})
+    filterRoutes();
+  }
 });
 
 App.controller('SidebarNav', function($scope) {
@@ -11368,11 +11385,54 @@ App.controller('Route', function($scope) {
     $('#Body').scope().activePage = 'console';
     $('#Consoles').scope().activeConsole = $scope.$index;
   }
+
+  $scope.addParameter = function() {
+    $scope.route.operation.parameters.push({in: 'query', name: 'myParam', type: 'string'})
+  }
+
+  $scope.removeParameter = function(idx) {
+    $scope.route.operation.parameters.splice(idx, 1);
+  }
+
+  $scope.addResponse = function() {
+    var code = 200;
+    while ($scope.route.operation.responses[String(code)]) ++code;
+    $scope.route.operation.responses[String(code)] = {}
+  }
+
+  $scope.removeResponse = function(code) {
+    delete $scope.route.operation.responses[code];
+  }
 })
+
+App.controller('EditMarkdown', function($scope) {})
 
 App.controller('Schema', function($scope) {
   $scope.printSchema = function(schema) {
-    return JSON.stringify(EXAMPLES.schemaExample(schema), null, 2);
+    return JSON.stringify(EXAMPLES.schemaExample($scope.schema), null, 2);
+  }
+  $scope.edit = function(schema) {
+    $scope.schemaString = JSON.stringify(schema, null, 2);
+    $scope.clicked = true;
+  }
+  $scope.save = function() {
+    try {
+      var ret = $scope.schemaString ? JSON.parse($scope.schemaString) : null;
+      $scope.clicked = false;
+      $scope.parseError = '';
+      return ret;
+    } catch (e) {
+      console.log('err')
+      $scope.parseError = e.message;
+    }
+  }
+  $scope.getString = function(schema) {
+    return JSON.stringify(schema, null, 2);
+  }
+  $scope.codemirrorLoad = function(editor) {
+    editor.on("change", function(ch) {
+      $scope.schemaString = ch.getValue();
+    });
   }
 })
 
@@ -11402,6 +11462,19 @@ App.controller('DocParameter', function($scope) {
   }
 })
 
+
+App.controller('ResponseCode', function($scope) {
+  var origCode = $scope.code;
+  console.log('orig', origCode);
+  $scope.save = function(code) {
+    if (code !== origCode) {
+      console.log('swag', origCode, code);
+      $scope.route.operation.responses[code] = $scope.route.operation.responses[origCode];
+      delete $scope.route.operation.responses[origCode];
+    }
+    return true;
+  }
+})
 
 var getSeparatorFromFormat = function(format) {
   if (!format || format === 'csv' || format === 'multi') {
