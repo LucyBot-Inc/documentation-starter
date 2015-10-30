8e8f5ebc9b0ddac4e001fe5aca0f7b74be2e74c4
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-10-30 16:28:41 +0000

    fix bearer token logic

--- static/js/ng/console.js
+++ static/js/ng/console.js
@@ -96,7 +96,7 @@ App.controller('Console', function($scope) {
           if (keys[sec.name]) addParam(sec, keys[sec.name]);
         } else if (sec.type === 'oauth2' && keys.oauth2 && !addedOauth) {
           if (sec.flow === 'implicit') params.query = {access_token: keys.oauth2};
-          else params.headers = {'Authorization': keys.oauth2}
+          else params.headers = {'Authorization': 'Bearer ' + keys.oauth2}
           addedOauth = true;
         } else if (sec.type === 'basic' && keys.username && keys.password) {
           params.headers = {'Authorization': 'Basic ' + btoa(keys.username + ':' + keys.password)};
