5b158e809013cf51de7d0647070f29b62eeb4bf9
Ubuntu <ubuntu@ip-10-0-0-252.us-west-2.compute.internal>
2016-01-08 18:42:12 +0000

    add strapping

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var FS = require('fs');
 var App = require('express')();
 var GalleryRouter = require('./index.js').GalleryRouter;
 
+var strapping = !process.env.DEVELOPMENT ? null : {
+  css: __dirname + '/static/css/bootstrap.css',
+  config: __dirname + '/static/css/bootstrap-config.css',
+}
+
 var router = new GalleryRouter({
   proxy: true,
   enableEditor: true,
@@ -9,10 +14,12 @@ var router = new GalleryRouter({
     name: 'hacker_news',
     swagger: JSON.parse(FS.readFileSync(__dirname + '/examples/hacker_news.json')),
   }],
+  cssIncludes: [__dirname + '/static/css/bootstrap.css'],
   galleryInfo: {
-    title: "My APIs"
+    title: "Sample APIs"
   },
   development: process.env.DEVELOPMENT ? true : false,
+  strapping: strapping,
 });
 
 App.use(router.router);
