6eaaf207ea3c2bd8dc5d247ea0bd2242aa8d3e5e
Ubuntu <ubuntu@ip-10-0-0-252.us-west-2.compute.internal>
2016-02-11 22:47:06 +0000

    logspam

--- static/js/ng/documentation.js
+++ static/js/ng/documentation.js
@@ -46,7 +46,6 @@ App.controller('Docs', function($scope, $location) {
   }
 
   $scope.$watch('menuItems.active', function() {
-    if ($scope.menuItems) console.log('setac', $scope.menuItems.active);
     var active = ($scope.menuItems || []).active;
     if (!active || !active.method) return;
     if ($scope.isActive('Documentation')) $location.path('/Documentation/' + active.method + '/' + encodeURIComponent(active.title));
