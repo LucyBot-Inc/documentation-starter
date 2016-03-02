27265dc6fc67ace3a47489c81ab87b29062f5985
Bobby Brennan <bobby.brennan@gmail.com>
2016-03-02 16:16:21 +0000

    minify

--- static/minified/js/console.js
+++ static/minified/js/console.js
@@ -16816,6 +16816,15 @@ App.controller('Portal', function($scope, $location, spec) {
 App.controller('README', function($scope) {
   $scope.README = $scope.spec.info['x-lucy/readme'] || $scope.spec.info.description;
   $scope.showSections = Array.isArray($scope.README);
+  if ($scope.showSections) {
+    $scope.menuItems = $scope.README.map(function(r, idx) {
+      for (var i = 0; i < 10; ++i) r.contents += idx + r.contents + ' ';
+      return {
+        title: r.title,
+        targetID: '#README' + idx,
+      }
+    })
+  }
 })
 
 App.controller('Docs', function($scope, $location) {
